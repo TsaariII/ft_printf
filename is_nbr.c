038de9aabceec07e9486841596a6948ec34c7dbe
nzharkev <nzharkev@student.hive.fi>
2024-05-15 11:37:01 +0300

    Created is_nbr

--- is_nbr.c
+++ is_nbr.c
@@ -1,25 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   is_nbr.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/09 13:26:29 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/05/15 11:36:45 by nzharkev         ###   ########.fr       */
+/*   Created: 2024/05/15 11:10:29 by nzharkev          #+#    #+#             */
+/*   Updated: 2024/05/15 11:36:08 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	FT_PRINTF_H
-# define FT_PRINTF_H
-# include <unistd.h>
-# include <stdarg.h>
+#include "ft_printf.h"
 
-int	check_f(char const *format, va_list args, int *c);
-int	f_id(char const *str, va_list args, int *c);
-int	ft_printf(char const *str, ...);
-int	is_char(int c, int *check);
-int	is_nbr(int n, int *check);
-
-
-#endif
+int	is_nbr(int n, int *check)
+{
+	int i;
+	
+	i = 0;
+	if (n == -2147483648)
+		i += write(1, "-2147483648", 11);
+	else if (n < 0)
+	{
+		i += is_char('-', check);
+		i += is_nbr(-n, check);
+	}
+	else if (n >= 10)
+	{
+		i += ft_nbr(n / 10, check);
+		i += ft_char(n % 10 + 48, check);
+	}
+	else
+		i += is_char(n % 10 + 48, check);
+	return (i);
+}
