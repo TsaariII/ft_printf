1c3ea4c65c834255359316b830c60d98577b94b3
nzharkev <nzharkev@student.hive.fi>
2024-05-15 13:28:43 +0300

    Created is_pointer

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 13:26:29 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/05/15 13:17:26 by nzharkev         ###   ########.fr       */
+/*   Updated: 2024/05/15 13:26:55 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@ int	check_f(char const *format, va_list args, int *c);
 int	f_id(char const *str, va_list args, int *c);
 int	ft_printf(char const *str, ...);
 int	is_char(int c, int *check);
-int is_hex(unsigned long lng, int cap, int *check);
+int	is_hex(unsigned long lng, int cap, int *check);
 int	is_nbr(int n, int *check);
-int is_unsignbr(unsigned int n, int *check);
+int	is_pointer(void *ptr, int check);
+int	is_unsignbr(unsigned int n, int *check);
 
 #endif
