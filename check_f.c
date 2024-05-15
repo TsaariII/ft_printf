bb42fd137ee04dd4281a51045a899d8b4f4c8e97
nzharkev <nzharkev@student.hive.fi>
2024-05-15 10:11:40 +0300

    First draft of these

--- check_f.c
+++ check_f.c
@@ -6,11 +6,37 @@
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 14:35:05 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/05/10 11:40:28 by nzharkev         ###   ########.fr       */
+/*   Updated: 2024/05/15 10:09:54 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	f_id(char const *str, va_list args, int *c)
+{
+	int i;
+	
+	i = 0;
+	if (*str == 'c')
+		i += ft_char(va_arg(args, int), c);
+	else if (*str == 's')
+		i += ft_str(va_arg(args, int), c);
+	else if (*str == 'd' || *str == 'i')
+		i += ft_nbr(va_arg(args, int), c);
+	else if (*str == '%')
+		i += ft_char('%', i);
+	else if (*str == 'X')
+		i += ft_hex(va_arg(args, unsigned int), i);
+	else if (*str == 'x')
+		i += ft_hex(va_arg(args, unsigned int), i);
+	else if (*str == 'u')
+		i += ft_unsignbr(va_arg(args, unsigned int), i);
+	else if (*str == 'p')
+		i += ft_pointer(va_arg(args, void *), i);
+	if (*str == -1)
+		return (-1);
+	return (i);
+}
 
 int	check_f(char const *format, va_list args, int *c)
 {
@@ -25,5 +51,12 @@ int	check_f(char const *format, va_list args, int *c)
 			if (*c == -1)
 				return (-1);
 		}
-	}	
+		else if (*format == '%')
+		{
+			format++;
+			n += f_id(format, args, c);
+		}
+		format++;
+	}
+	return (n);
 }
--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,27 @@
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 11:00:50 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/05/09 13:30:09 by nzharkev         ###   ########.fr       */
+/*   Updated: 2024/05/14 10:59:07 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(char const *format, ...)
+int	ft_printf(char const *str, ...)
 {
-	
+	int		n;
+	int		c;
+	va_list	args;
+
+	n = 0;
+	c = 0;
+	va_start(args, str);
+	n = check_f(str, args, &c);
+	if (c == -1)
+	{
+		va_end(args);
+		return(-1);
+	}
+	va_end(args);
+	return (n);
 }
